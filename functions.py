d06f6c79371da74f187ddaf8dbc852d6cce2f7f2
project@pkt5.com <project@pkt5.com@85a7cb5e-0b71-58f5-ebbc-142c6466654b>
2013-04-08 22:28:27 +0000

    Removed useless functions, rethink, remake!

--- functions.py
+++ functions.py
@@ -1,25 +1,23 @@
-# import me to main.py!
 from scapy.all import *
 import Queue
 
-Ether_comms_Q = Queue.Queue(10000)
+pkt_Q = Queue.Queue(10000)
 
 # Processing of packet to save to the queues
 def processpkt(pkt):
     
     if pkt == None:
-        Ether_comms_Q.put_nowait(pkt)
+        pkt_Q.put_nowait(pkt)
+        print '[!]\tEnd of data'
         return    
     
     if not pkt.name == 'Ethernet':
         return    
     
     try:
-        Ether_comms_Q.put_nowait(pkt)
-        
+        pkt_Q.put_nowait(pkt)
     except Queue.Full:
-        
-        print '*** TRAFFIC OVERLOAD ***\n*** Dropped packet... Sorry! ***'
+        print '*** TRAFFIC OVERLOAD ***'
         return        
 
 # Gets a list of dev's (interfaces) - Rough, doesn't work for windows
--- main.py
+++ main.py
@@ -1,54 +1,58 @@
 #! /usr/bin/env python
 import sys, threading
+sys.dont_write_bytecode = True
 
 from scapy.all import *
 from functions import *
 from queue_process import *
 
-# from optparse import OptionParser
-# https://github.com/pentestmonkey/gateway-finder/blob/master/gateway-finder.py
+def main(arg):
 
-
-if len(sys.argv) == 1:
-    print 'Error, no arguments'
-    print 'Usage: %s <pcap file|dev>' % sys.argv[0]
-    sys.exit(1)
+    # from optparse import OptionParser
+    # https://github.com/pentestmonkey/gateway-finder/blob/master/gateway-finder.py
     
+    if len(arg) == 1:
+        print 'Error, no arguments'
+        print 'Usage: %s <pcap file|dev>' % arg[0]
+        sys.exit(1)
 
-src = sys.argv[1]
+    src = arg[1]
+    
+    # Process queues
+    qthread = threading.Thread(target=processQ)
+    qthread.setDaemon(True)
+    qthread.start()
+    
+    # Get the interfaces list
+    devs = getinterfaces()
+    
+    if devs.__contains__(src):
+        isdev = True    
+    else:
+        isdev = False
+    
+    if isdev:
+        print '[-]\tSniffing...'
+        try:
+            sniff(store=0, prn=lambda x:processpkt(x), iface=src)
+        except KeyboardInterrupt:
+            print '[+]\tStopped sniffing!'
+    else:
+        print '[-]\tReading file %s' % str(src)
+        try:    
+            sniff(store=0, prn=lambda x:processpkt(x), offline=src, count=10000)
+            print '[+]\tFinished reading file %s!' % str(src)
+        except KeyboardInterrupt:
+            print '[+]\tUser stopped reading file!'
+    
+    # Notify sniffing has ended
+    processpkt(None)
+    
+    # Wait for processing thread to complete all tasks in queue
+    qthread.join()
 
-# Process queues
-qthread = threading.Thread(target=processQs)
-
-qthread.setDaemon(True)
-qthread.start()
-
-# Get the interfaces list
-devs = getinterfaces()
-
-if devs.__contains__(src):
-    isdev = True    
+    
+if __name__ == "__main__":
+    main(sys.argv)
 else:
-    isdev = False
-
-if isdev:
-    print '[-]\tSniffing...'
-    try:
-        sniff(store=0, prn=lambda x:processpkt(x), iface=src)#, count=10000)
-    except KeyboardInterrupt:
-        print '[+]\tStopped sniffing!'
-else:
-    print '[-]\tReading file %s' % str(src)
-    try:    
-        sniff(store=0, prn=lambda x:processpkt(x), offline=src)#, count=10000)
-        print '[+]\tFinished reading file %s!' % str(src)
-    except KeyboardInterrupt:
-        print '[+]\tUser stopped reading file!'
-
-# Notify sniffing has ended
-processpkt(None)
-
-# Wait for processing thread to complete all tasks in queue
-qthread.join()
-
-createmap()
+    print '\nPlease run me as "main", thanks :)'
